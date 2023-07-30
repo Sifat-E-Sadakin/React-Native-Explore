038d8f88ca72772fb3d20c2c67ed8534c117f015
Sifat-E-Sadakin <sifatesadakin10@gmail.comm>
2023-07-31 02:12:24 +0600

    navbar implemented

--- app/index.js
+++ app/index.js
@@ -1,11 +1,41 @@
-import { View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { COLORS, icons, images, SIZES } from "../constants";
 
+import {ScreenHeaderBtn, Welcome, Nearbyjobs, Popularjobs} from "../components";
 
 const index = () => {
+
+    let router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }} >
+            <Stack.Screen options={{
+                headerStyle:
+                    { backgroundColor: COLORS.lightWhite },
+                headerShadowVisible: false,
+                headerLeft: ()=> (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" ></ScreenHeaderBtn>
+                ),
+                headerRight: ()=> (
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" ></ScreenHeaderBtn>
+                ),
+                headerTitle: ""
+            }} >
+
+
+            </Stack.Screen>
+
+            <ScrollView>
+                <View style={{flex: 1, padding: SIZES.medium}}>
+                   <Welcome></Welcome>
+                   <Popularjobs></Popularjobs>
+                   <Nearbyjobs></Nearbyjobs>
+                </View>
+            </ScrollView>
+
+
+        </SafeAreaView>
     );
 };
 
